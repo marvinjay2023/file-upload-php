4f8086378fdcffc29451b2de47276f2738b1026c
Marvin Rubio <marvinjay.rubio@ssu.edu.ph>
2025-01-29 20:27:00 +0800

    created a default profile for the user

--- partials/nav.php
+++ partials/nav.php
@@ -18,9 +18,9 @@
                 <div class="d-flex justify-content-end">
                   <div class="flex-shrink-0 dropdown">
                     <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle text-white" data-bs-toggle="dropdown" aria-expanded="false">
-                      <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                      <img src="files/profile/default.jpg" alt="profile" width="32" height="32" class="rounded-circle">
                     </a>
-                    <ul class="dropdown-menu text-small shadow" style="">
+                    <ul class="dropdown-menu text-small shadow">
                       <li><a class="dropdown-item" href="#">Profile</a></li>
                       <li><hr class="dropdown-divider"></li>
                       <li><a class="dropdown-item" href="#">Sign out</a></li>
